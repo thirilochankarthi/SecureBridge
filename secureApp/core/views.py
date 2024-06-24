453cd24bd42286fc439d84a311f56aa5d692f09f
THIRILOCHAN KARTHI M S <thirilochankarthi@gmail.com>
2024-06-24 22:05:53 +0530

    Login and signup page built

--- secureApp/core/views.py
+++ secureApp/core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def login(request):
+    return render(request, 'login.html')
+
+def signup_view(request):
+    return render(request, 'signup.html')
+
+def home_view(request):
+    return HttpResponse("Welcome to Home")
